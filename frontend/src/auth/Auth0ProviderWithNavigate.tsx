daeee982711dfc086e60333ebb23575c9012e744
Subodh Rana <40374844+Subodhrana390@users.noreply.github.com>
2024-04-16 00:08:28 +0530

    auth0

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,4 +1,4 @@
-import { AppState, Auth0Provider, User, useAuth0 } from "@auth0/auth0-react";
+import { Auth0Provider } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
 type Props = {
